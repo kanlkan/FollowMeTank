953a48314c0bc2e4408755680428af9cd19bbab1
Masaharu Kanda <kanlkan.naklnak@gmail.com>
2022-01-23 17:56:25 +0900

    add helper script

---
+++ tool/print_gpio.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo gpio12
+cat /sys/class/gpio/gpio12/value
+echo gpio13
+cat /sys/class/gpio/gpio13/value
+echo gpio14
+cat /sys/class/gpio/gpio14/value
+echo gpio15
+cat /sys/class/gpio/gpio15/value
