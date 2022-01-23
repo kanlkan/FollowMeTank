04e8b654d29d828c4a37edc9717810cda761bda4
Masaharu Kanda <kanlkan.naklnak@gmail.com>
2022-01-23 17:55:23 +0900

    change the way for moving tank (DRV8335)

--- src/control_tank.py
+++ src/control_tank.py
@@ -1,15 +1,15 @@
+import logging
 import os
 import subprocess
 import threading
 import time
-import logging
 
 logger = logging.getLogger(__name__)
 
 
-class ControlTank():
-    PWM_DUTY_A: float = 0.5
-    PWM_DUTY_B: float = 0.5
+class ControlTank:
+    PWM_DUTY_A: float = 0.9
+    PWM_DUTY_B: float = 0.9
 
     def __init__(self) -> None:
         self._enable_a: bool = False
@@ -22,26 +22,26 @@ class ControlTank():
         if ret is False:
             return False
 
-        self._start_pwm_threads()
+        # self._start_pwm_threads()
         return True
 
     def _setup_gpio(self) -> bool:
         # Map GPIO12, GPIO13, GPIO14, GPIO15 (pin 37, 22, 13, 18)
-        with open('/sys/class/gpio/export', 'w') as dev_file:
-            if os.path.exists('/sys/class/gpio/gpio12') is False:
-                ret = subprocess.run(['echo', '12'], stdout=dev_file)
+        with open("/sys/class/gpio/export", "w") as dev_file:
+            if os.path.exists("/sys/class/gpio/gpio12") is False:
+                ret = subprocess.run(["echo", "12"], stdout=dev_file)
                 if ret.returncode != 0:
                     return False
-            if os.path.exists('/sys/class/gpio/gpio13') is False:
-                ret = subprocess.run(['echo', '13'], stdout=dev_file)
+            if os.path.exists("/sys/class/gpio/gpio13") is False:
+                ret = subprocess.run(["echo", "13"], stdout=dev_file)
                 if ret.returncode != 0:
                     return False
-            if os.path.exists('/sys/class/gpio/gpio14') is False:
-                ret = subprocess.run(['echo', '14'], stdout=dev_file)
+            if os.path.exists("/sys/class/gpio/gpio14") is False:
+                ret = subprocess.run(["echo", "14"], stdout=dev_file)
                 if ret.returncode != 0:
                     return False
-            if os.path.exists('/sys/class/gpio/gpio15') is False:
-                ret = subprocess.run(['echo', '15'], stdout=dev_file)
+            if os.path.exists("/sys/class/gpio/gpio15") is False:
+                ret = subprocess.run(["echo", "15"], stdout=dev_file)
                 if ret.returncode != 0:
                     return False
 
@@ -49,36 +49,116 @@ class ControlTank():
         time.sleep(3)
 
         # Set direction as out on GPIO12 (pin 37)
-        with open('/sys/class/gpio/gpio12/direction', 'w') as dev_file:
-            ret = subprocess.run(['echo', 'out'], stdout=dev_file)
+        with open("/sys/class/gpio/gpio12/direction", "w") as dev_file:
+            ret = subprocess.run(["echo", "out"], stdout=dev_file)
             if ret.returncode != 0:
                 return False
 
         # Set direction as out on GPIO13 (pin 22)
-        with open('/sys/class/gpio/gpio13/direction', 'w') as dev_file:
-            ret = subprocess.run(['echo', 'out'], stdout=dev_file)
+        with open("/sys/class/gpio/gpio13/direction", "w") as dev_file:
+            ret = subprocess.run(["echo", "out"], stdout=dev_file)
             if ret.returncode != 0:
                 return False
 
         # Set direction as out on GPIO14 (pin 13)
-        with open('/sys/class/gpio/gpio14/direction', 'w') as dev_file:
-            ret = subprocess.run(['echo', 'out'], stdout=dev_file)
+        with open("/sys/class/gpio/gpio14/direction", "w") as dev_file:
+            ret = subprocess.run(["echo", "out"], stdout=dev_file)
             if ret.returncode != 0:
                 return False
 
         # Set direction as out on GPIO15 (pin 18)
-        with open('/sys/class/gpio/gpio15/direction', 'w') as dev_file:
-            ret = subprocess.run(['echo', 'out'], stdout=dev_file)
+        with open("/sys/class/gpio/gpio15/direction", "w") as dev_file:
+            ret = subprocess.run(["echo", "out"], stdout=dev_file)
             if ret.returncode != 0:
                 return False
 
         return True
 
     def _set_gpio(self, addr: int, value: int) -> bool:
-        with open(f'/sys/class/gpio/gpio{addr}/value', 'w') as dev_file:
-            ret = subprocess.run(['echo', f'{value}'], stdout=dev_file)
+        with open(f"/sys/class/gpio/gpio{addr}/value", "w") as dev_file:
+            ret = subprocess.run(["echo", f"{value}"], stdout=dev_file)
             if ret.returncode != 0:
                 return False
+        return True
+
+    def move_forward(self) -> bool:
+        ret = self._set_gpio(12, 0)
+        if ret is False:
+            return False
+        ret = self._set_gpio(13, 1)
+        if ret is False:
+            return False
+        ret = self._set_gpio(14, 0)
+        if ret is False:
+            return False
+        ret = self._set_gpio(15, 1)
+        if ret is False:
+            return False
+
+        return True
+
+    def move_backward(self) -> bool:
+        ret = self._set_gpio(12, 1)
+        if ret is False:
+            return False
+        ret = self._set_gpio(13, 0)
+        if ret is False:
+            return False
+        ret = self._set_gpio(14, 1)
+        if ret is False:
+            return False
+        ret = self._set_gpio(15, 0)
+        if ret is False:
+            return False
+
+        return True
+
+    def trun_clockwise(self) -> bool:
+        ret = self._set_gpio(12, 1)
+        if ret is False:
+            return False
+        ret = self._set_gpio(13, 0)
+        if ret is False:
+            return False
+        ret = self._set_gpio(14, 0)
+        if ret is False:
+            return False
+        ret = self._set_gpio(15, 1)
+        if ret is False:
+            return False
+
+        return True
+
+    def trun_counterclockwise(self) -> bool:
+        ret = self._set_gpio(12, 0)
+        if ret is False:
+            return False
+        ret = self._set_gpio(13, 1)
+        if ret is False:
+            return False
+        ret = self._set_gpio(14, 1)
+        if ret is False:
+            return False
+        ret = self._set_gpio(15, 0)
+        if ret is False:
+            return False
+
+        return True
+
+    def stop(self) -> bool:
+        ret = self._set_gpio(12, 1)
+        if ret is False:
+            return False
+        ret = self._set_gpio(13, 1)
+        if ret is False:
+            return False
+        ret = self._set_gpio(14, 1)
+        if ret is False:
+            return False
+        ret = self._set_gpio(15, 1)
+        if ret is False:
+            return False
+
         return True
 
     def _start_pwm_threads(self) -> None:
@@ -88,85 +168,85 @@ class ControlTank():
     def _pwm_a_loop(self) -> None:
         while True:
             if self._enable_a is True:
-                self._set_gpio(12, 1)
+                self._set_gpio(13, 1)
                 time.sleep(ControlTank.PWM_DUTY_A / 10)
-                self._set_gpio(12, 0)
+                self._set_gpio(13, 0)
                 time.sleep(0.1 - ControlTank.PWM_DUTY_A / 10)
             else:
-                self._set_gpio(12, 0)
+                self._set_gpio(13, 0)
 
     def _pwm_b_loop(self) -> None:
         while True:
             if self._enable_b is True:
-                self._set_gpio(14, 1)
+                self._set_gpio(15, 1)
                 time.sleep(ControlTank.PWM_DUTY_B / 10)
-                self._set_gpio(14, 0)
+                self._set_gpio(15, 0)
                 time.sleep(0.1 - ControlTank.PWM_DUTY_B / 10)
             else:
-                self._set_gpio(14, 0)
+                self._set_gpio(15, 0)
 
     def _set_moter_direction_a(self, value: int):
-        return self._set_gpio(13, value)
+        return self._set_gpio(12, value)
 
     def _set_moter_direction_b(self, value: int):
-        return self._set_gpio(15, value)
+        return self._set_gpio(14, value)
 
-    def move_forward(self) -> None:
+    def move_forward_pwm(self) -> None:
         ret = self._set_moter_direction_a(0)
         if ret is False:
-            logger.error('failed to move_forward')
+            logger.error("failed to move_forward")
             return
         ret = self._set_moter_direction_b(0)
         if ret is False:
-            logger.error('failed to move_forward')
+            logger.error("failed to move_forward")
             return
 
         self._enable_a = True
         self._enable_b = True
 
-    def move_backward(self) -> None:
+    def move_backward_pwm(self) -> None:
         ret = self._set_moter_direction_a(1)
         if ret is False:
-            logger.error('failed to move_backward')
+            logger.error("failed to move_backward")
             return
         ret = self._set_moter_direction_b(1)
         if ret is False:
-            logger.error('failed to move_backward')
+            logger.error("failed to move_backward")
             return
 
         self._enable_a = True
         self._enable_b = True
 
-    def turn_clockwise(self) -> None:
+    def turn_clockwise_pwm(self) -> None:
         ret = self._set_moter_direction_a(1)
         if ret is False:
-            logger.error('failed to turn_clockwise')
+            logger.error("failed to turn_clockwise")
             return
         ret = self._set_moter_direction_b(0)
         if ret is False:
-            logger.error('failed to turn_clockwise')
+            logger.error("failed to turn_clockwise")
             return
 
         self._enable_a = True
         self._enable_b = True
 
-    def turn_counterclockwise(self) -> None:
+    def turn_counterclockwise_pwm(self) -> None:
         ret = self._set_moter_direction_a(0)
         if ret is False:
-            logger.error('failed to turn_counterclockwise')
+            logger.error("failed to turn_counterclockwise")
             return
         ret = self._set_moter_direction_b(1)
         if ret is False:
-            logger.error('failed to turn_counterclockwise')
+            logger.error("failed to turn_counterclockwise")
             return
 
         self._enable_a = True
         self._enable_b = True
 
-    def stop(self) -> None:
+    def stop_pwm(self) -> None:
         self._enable_a = False
         self._enable_b = False
 
-    def finalize(self) -> None:
+    def finalize_pwm(self) -> None:
         self._pwm_thread_a.join()
         self._pwm_thread_b.join()
