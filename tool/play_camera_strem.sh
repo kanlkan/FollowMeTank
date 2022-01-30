8541c4952c248fc525ffeba19a28269dbb57c838
Masaharu Kanda <kanlkan.naklnak@gmail.com>
2022-01-30 21:35:13 +0900

    fix for playing camera stream tool

--- tool/play_camera_strem.sh
+++ tool/play_camera_strem.sh
@@ -1,1 +1,3 @@
-gst-launch-1.0 nvarguscamerasrc ! nvoverlaysink
+#gst-launch-1.0 nvarguscamerasrc ! nvoverlaysink
+#gst-launch-1.0 nvarguscamerasrc sensor_mode=2 ! nvvidconv ! video/x-raw, format=BGRx ! videoconvert ! nvoverlaysink
+gst-launch-1.0 nvarguscamerasrc sensor_mode=2 ! nvvidconv ! video/x-raw, format=I420 ! videoconvert ! nvoverlaysink
